1d31cfe98127cebdce139d1a7472c25e4d6b41c4
Daniel Spencer <dms305@hku.hk>
2019-05-24 17:58:35 +0800

    Properly expose public API in __init__.py

--- pepfrag/__init__.py
+++ pepfrag/__init__.py
@@ -1,3 +1,14 @@
 from .constants import AA_MASSES, FIXED_MASSES, Mass, MassType
 from .ion_generators import Ion, IonType
 from .pepfrag import ModSite, Peptide
+
+__all__ = [
+    "AA_MASSES",
+    "FIXED_MASSES",
+    "Mass",
+    "MassType",
+    "Ion",
+    "IonType",
+    "ModSite",
+    "Peptide",
+]
