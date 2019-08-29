414fece2403dd81a69e6a2328a0abd9413d5d6a1
Daniel Spencer <dms305@hku.hk>
2019-08-29 23:38:32 +0800

    Update setup.py to use -std=c++11 on MacOS

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from distutils.core import setup, Extension
 import os
+import sys
 
 PACKAGE_DIR = "pepfrag"
+
+extra_compiler_args = []
+if sys.platform == "darwin":
+    extra_compiler_args.append("-std=c++11")
+
 
 cpepfrag = Extension(
     "cpepfrag",
@@ -9,7 +15,9 @@ cpepfrag = Extension(
         os.path.join(PACKAGE_DIR, "iongenerator.cpp"),
         os.path.join(PACKAGE_DIR, "converters.cpp"),
         os.path.join(PACKAGE_DIR, "cpepfrag.cpp"),
-    ]
+    ],
+    language="c++11",
+    extra_compile_args=extra_compiler_args
 )
 
 setup(
